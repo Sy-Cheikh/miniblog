5b66648da9aa7bdcc8b360c3259837c53934de1d
SyCheikh <sycheikh1213@gmail.com>
2022-08-18 18:41:43 +0200

    pushing the existing project

---
+++ src/useFetch.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        setTimeout(() =>{
+            fetch(url)
+            .then(res =>{
+                if(!res.ok){
+                    throw Error ('Could not fatch the data for that resource');
+                 }
+                 
+                return res.json();
+            })
+            .then(data =>{
+                setData(data);
+                setLoading(false);
+                setError(null);
+            })
+            .catch(err =>{
+                setLoading(false);
+                setError(err.message);
+            })
+            
+        },1000);
+    },[url]);
+
+    return {data, isLoading, error}
+
+}
+export default useFetch;
